d229ecf1ba87469e5e0ffc1bb300581b801b1c8e
jz <xiejiangzheng@gmail.com>
2025-01-12 09:42:43 +0800

    [feat] add backend config

--- backend/config.py
+++ backend/config.py
@@ -1,21 +1,33 @@
-from pydantic_settings import BaseSettings, SettingsConfigDict
-# from fastapi import 
 from functools import lru_cache
 
-class AuthSettings(BaseSettings):
-    client_id: str
-    client_secret: str
+from pydantic_settings import BaseSettings, SettingsConfigDict
+from pydantic import SecretStr
+
+class OpenSearchSettings(BaseSettings):
+    host:str
+    port:int
+    user:str
+    password: SecretStr
+
+class OllamaSettings(BaseSettings):
+    host:str
+
+class OAuthSettings(BaseSettings):
+    client_id:str
+    client_secret:str
+    redirect_uri:str
+
+class Settings(BaseSettings):
+    ollama: OllamaSettings
+    opensearch: OpenSearchSettings
+    oauth: OAuthSettings
     
-    model_config = SettingsConfigDict(env_file=".env")
+    model_config = SettingsConfigDict(env_file='.env', env_nested_delimiter="__")
 
 @lru_cache
-def get_auth_settings():
-    return AuthSettings().model_dump()
-# def get_auth_config(request:Request):
-#     return {
-#         "response_type": "code",
-#         # "client_id": AuthSettings.model_config.get("CLIENT_ID"),
-#         "client_id": "6j7nry5cequaFGGqqqBTTd1e7oy4M9pVb6bckVGa",
-#         "redirect_uri": "http://127.0.0.1:8000/callback"
-#         # "redirect_uri": f"{request.base_url}/{app.url_path_for('callback')}"
-#     }
+def get_settings():
+    return Settings()
+
+
+if __name__ == '__main__':
+    print(get_settings().model_dump())
