0d9072bea5da0d713c84a4752885a28de2936b11
jz <xiejiangzheng@gmail.com>
2025-01-14 21:56:43 +0800

    [feat] refactor backend settings

--- backend/auth.py
+++ backend/auth.py
@@ -8,7 +8,9 @@ from pydantic import BaseModel
 from typing_extensions import Annotated
 import httpx
 import urllib
-from config import get_auth_settings
+from config import get_settings
+
+oauth_settings = get_settings().oauth
 
 app = FastAPI()
 
@@ -19,12 +21,10 @@ app = FastAPI()
 
 
 
-auth_url="http://localhost:9000/application/o/authorize/"
-token_url = "http://localhost:9000/application/o/token/"
 
 oauth2_scheme = OAuth2AuthorizationCodeBearer(
-    authorizationUrl=auth_url,
-    tokenUrl=token_url,
+    authorizationUrl=oauth_settings.auth_url,
+    tokenUrl=oauth_settings.token_url,
 )
 
 # @lru_cache
@@ -42,20 +42,58 @@ oauth2_scheme = OAuth2AuthorizationCodeBearer(
 # # redirect_uri = "https://yourapp.com/callback"
 # # oauth2_scheme.
 
+def login():
+    oauth_settings = settings.oauth
+    
+    param = {
+        "response_type": "code",
+        "client_id": oauth_settings.client_id,
+        "redirect_uri":  oauth_settings.redirect_uri
+        }
+    auth_link = f"{auth_url}?{urllib.parse.urlencode(param)}"
+    st.markdown(
+        f'<a href="{auth_link}" target="_self" style="display: inline-block; padding: 12px 20px; background-color: #4CAF50; color: white; text-align: center; text-decoration: none; font-size: 16px; border-radius: 4px;">Login with Snowflake</a>',
+        unsafe_allow_html=True
+    )
+    
+    if st.query_params.get("code", False):
+        code = st.query_params["code"]
+        param.update({
+            "grant_type": "authorization_code",
+            "client_secret": oauth_settings.client_secret,
+            "code": code
+            
+        })
+        r = httpx.post(url=token_url,data=param)
+        if r.status_code == 200:
+            # print(True)/
+            st.session_state["logged_in"]=True
+            st.switch_page("pages/chat.py")
+
 
 @app.get("/login")
-async def login(config: Annotated[dict, Depends(get_auth_settings)], request: Request):
+async def login(config: Annotated[dict, Depends(get_settings)], request: Request):
     """Redirect to OAuth 2.0 server for login."""
-    auth_link = f"{auth_url}?{urllib.parse.urlencode(config)}"
-    print(config)
+    param = {
+        "response_type": "code",
+        "client_id": oauth_settings.client_id,
+        "redirect_uri":  oauth_settings.redirect_uri
+        }
+    auth_link = f"{oauth_settings.auth_url}?{urllib.parse.urlencode(param)}"
     return RedirectResponse(auth_link)
 
 @app.get("/callback")
 async def callback(code: str):
     """Redirect to OAuth 2.0 server for login."""
-    print(get_auth_settings())
+    param = {
+        "response_type": "code",
+        "client_id": oauth_settings.client_id,
+        "client_secret": oauth_settings.client_secret,
+        "code": code
+    }
+    
     httpx.post(
-        url=token_url,
+        url=oauth_settings.token_url,
         # data={
         #     "code": code
         #     "client_id":
--- backend/config.py
+++ backend/config.py
@@ -3,31 +3,45 @@ from functools import lru_cache
 from pydantic_settings import BaseSettings, SettingsConfigDict
 from pydantic import SecretStr
 
+
 class OpenSearchSettings(BaseSettings):
-    host:str
-    port:int
-    user:str
-    password: SecretStr
+    host: str
+    port: int
+    user: str = ""
+    password: SecretStr = SecretStr("")
+
 
 class OllamaSettings(BaseSettings):
-    host:str
+    host: str
+
 
 class OAuthSettings(BaseSettings):
-    client_id:str
-    client_secret:str
-    redirect_uri:str
+    auth_url: str
+    token_url: str
+    redirect_uri: str
+    client_id: str
+    client_secret: str
+
+class PostgresSettings(BaseSettings):
+    db: str
+    host: str
+    port: int
+    user: str = ""
+    password: SecretStr = SecretStr("")
 
 class Settings(BaseSettings):
     ollama: OllamaSettings
     opensearch: OpenSearchSettings
     oauth: OAuthSettings
-    
-    model_config = SettingsConfigDict(env_file='.env', env_nested_delimiter="__")
+    db: PostgresSettings
+
+    model_config = SettingsConfigDict(env_file=".env", env_nested_delimiter="__")
+
 
 @lru_cache
 def get_settings():
     return Settings()
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     print(get_settings().model_dump())
